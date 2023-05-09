f499c2f1c5cac577d033491c4a179a97968666dc
software groups <admin@sfgroups.com>
2023-05-09 08:21:41 -0400

    updated to code work with pu

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI, Depends, status, Response , HTTPException, Request
-from . import schemas,models
-from .database import engine, SessionLocal
-from sqlalchemy.orm import Session
+from pathlib import Path
+
 import uvicorn
+from fastapi import FastAPI, Depends, status, Response, HTTPException
+from sqlalchemy.orm import Session
+
+import blog.models as models
+import blog.schemas as schemas
+from blog.database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
+
 
 def get_db():
     db = SessionLocal()
@@ -17,8 +22,8 @@ def get_db():
 
 
 @app.post('/blog', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db : Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title,body=request.body)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
@@ -26,26 +31,24 @@ def create(request: schemas.Blog, db : Session = Depends(get_db)):
 
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id, db : Session = Depends(get_db)):
+def destroy(id, db: Session = Depends(get_db)):
     db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
 
     db.commit()
 
-
     return 'done'
 
-@app.put('/blog/{id}', status_code =status.HTTP_202_ACCEPTED)
-def update(id, request : schemas.Blog, db : Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).update(request)
+
+@app.put('/blog/{book_id}', status_code=status.HTTP_202_ACCEPTED)
+def update(book_id, request: schemas.Blog, db: Session = Depends(get_db)):
+    print(book_id)
+    print(request)
+    blog: models.Blog = db.query(models.Blog).filter(models.Blog.id == book_id).first()
+    blog.title = request.title
+    blog.body = request.body
     db.commit()
-    
+
     return 'updated'
-
-
-
-
-
-
 
 
 @app.get('/blog')
@@ -55,8 +58,12 @@ def all(db: Session = Depends(get_db)):
 
 
 @app.get('/blog/{id}')
-def show(id, response: Response, db: Session = Depends(get_db), status_code = 200):
+def show(id, response: Response, db: Session = Depends(get_db), status_code=200):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f'Blog with the id {id} is not available')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with the id {id} is not available')
     return blog
+
+
+if __name__ == "__main__":
+    uvicorn.run(f"{Path(__file__).stem}:app", host="0.0.0.0", port=8080, log_level="info", reload=True, workers=2)
